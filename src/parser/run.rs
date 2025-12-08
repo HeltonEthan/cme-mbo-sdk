06d9b5f2b138377a8b76b4eea39c66752a0ad8d3
HeltonEthan <HeltonEthan07@gmail.com>
2025-12-08 00:52:58 -0500

    minor additions, moved some things around

--- src/parser/run.rs
+++ src/parser/run.rs
@@ -1,1 +1,28 @@
+use color_eyre::eyre::Result;
+use std::path::PathBuf;
+use crate::parser::dbn;
 
+pub fn run(path: &PathBuf) -> Result<()> {
+    dbn::dbn_stream(path)?;
+
+    Ok(())
+}
+
+#[cfg(test)]
+pub mod test {
+    use super::*;
+    use crate::Config;
+
+    #[test]
+    fn run_test() -> Result<()> {
+        let path = PathBuf::from(
+            r"C:/Users/helto/GLBX-20250915-NGKNUL4VBG/glbx-mdp3-20250512-20250517.mbo.dbn.zst"
+        );
+        let mut config: Config = Default::default();
+        config.path(&path);
+
+        run(&config.path)?;
+
+        Ok(())
+    }
+}
